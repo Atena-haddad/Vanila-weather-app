f7ae362813fb7b63c1c7f123bc6e5b80c542240a
Atena-haddad <atenahaddad0@gmail.com>
2023-09-14 18:05:04 +0330

    implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -31,10 +31,11 @@ function displayTemperature(response){
   let windElement=document.querySelector("#wind");
   let dateElement=document.querySelector("#date");
   let iconElement=document.querySelector("#icon");
+  celsiusTemperature = response.data.main.temp;
   
 
 
-  temperatureElement.innerHTML=Math.round(response.data.main.temp);
+  temperatureElement.innerHTML=Math.round(celsiusTemperature);
   cityElement.innerHTML=response.data.name;
   descriptionElement.innerHTML=response.data.weather[0].description;
   humidityElement.innerHTML=response.data.main.humidity;
@@ -56,11 +57,33 @@ function handlesubmit(event){
   let cityInputElement=document.querySelector("#city-input");
   search(cityInputElement.value);
 }
+function displayFahrenheitTemperature(event){
+  event.preventDefault();
+  let fahrenheitTemperature=(celsiusTemperature*9)/5+32;
+  // remove the active class to the celsius link
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let temperatureElement=document.querySelector("#temperature");
+  temperatureElement.innerHTML=Math.round(fahrenheitTemperature);
+}
+function displayCelsiusTemperature(event){
+  event.preventDefault();
+  let temperatureElement=document.querySelector("#temperature");
+  temperatureElement.innerHTML=Math.round(celsiusTemperature);
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
 
 
-
-
-
+}
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit",handlesubmit);
+
+let fahrenheitLink=document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click",displayFahrenheitTemperature);
+
+let celsiusLink=document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click",displayCelsiusTemperature);
+
+search("New York");
