7f424f4c25e2f836ec77f79faf08451c9ca4e20b
Atena-haddad <atenahaddad0@gmail.com>
2023-09-14 14:50:41 +0330

    showing the current temperature

---
+++ src/app.js
@@ -0,0 +1,23 @@
+function displayTemperature(response){
+  let temperatureElement=document.querySelector("#temperature");
+  let cityElement=document.querySelector("#city");
+  let descriptionElement=document.querySelector("#description");
+  let humidityElement=document.querySelector("#humidity");
+  let windElement=document.querySelector("#wind");
+  temperatureElement.innerHTML=Math.round(response.data.main.temp);
+  cityElement.innerHTML=response.data.name;
+  descriptionElement.innerHTML=response.data.weather[0].description;
+  humidityElement.innerHTML=response.data.main.humidity;
+  windElement.innerHTML=Math.round(response.data.wind.speed);
+  console.log(response.data);
+}
+
+
+
+
+
+let apiKey="ab13aa3fe6d8d25130a60034078e4897";
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
