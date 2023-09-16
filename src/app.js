f686b80db3745f3019fafff3e2bdc049c61e23b7
Atena-haddad <atenahaddad0@gmail.com>
2023-09-16 14:52:43 +0330

    changed current weather api from "Openweather" to "Shecodes"

--- src/app.js
+++ src/app.js
@@ -64,7 +64,7 @@ function displayForecast(response){
 
 function getForecast(coordinates){
   let apiKey="a0c924feao2c9e30bt4b9f31c1337af3";
-  let apiUrl=`https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.lon}&lat=${coordinates.lat}&key=${apiKey}`;
+  let apiUrl=`https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}`;
   axios.get(apiUrl).then(displayForecast);
 
 }
@@ -77,26 +77,26 @@ function displayTemperature(response){
   let windElement=document.querySelector("#wind");
   let dateElement=document.querySelector("#date");
   let iconElement=document.querySelector("#icon");
-  celsiusTemperature = response.data.main.temp;
+  celsiusTemperature = response.data.temperature.current;
   
 
 
   temperatureElement.innerHTML=Math.round(celsiusTemperature);
-  cityElement.innerHTML=response.data.name;
-  descriptionElement.innerHTML=response.data.weather[0].description;
-  humidityElement.innerHTML=response.data.main.humidity;
+  cityElement.innerHTML=response.data.city;
+  descriptionElement.innerHTML=response.data.condition.description;
+  humidityElement.innerHTML=response.data.temperature.humidity;
   windElement.innerHTML=Math.round(response.data.wind.speed);
-  dateElement.innerHTML=formatDate(response.data.dt *1000);
-  iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  dateElement.innerHTML=formatDate(response.data.time *1000);
+  iconElement.setAttribute("src",`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
-  iconElement.setAttribute("alt",response.data.weather[0].description);
+  iconElement.setAttribute("alt",response.data.condition.description);
 
-  getForecast(response.data.coord);
+  getForecast(response.data.coordinates);
 }
 
 function search(city){
-  let apiKey="ab13aa3fe6d8d25130a60034078e4897";
-  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiKey="a0c924feao2c9e30bt4b9f31c1337af3";
+  let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(displayTemperature);
 
 }
@@ -109,6 +109,5 @@ function handlesubmit(event){
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit",handlesubmit);
-
 
 search("Tehran");
